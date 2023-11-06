29a07e88f419d34ffaf435b0f2bc769d33d3bdfa
PaulOrmaza <steven.ormaza816@ist17dejulio.edu.ec>
2023-11-06 13:21:17 -0500

    MI PRIMEER APLICATIVO MOVIL

---
+++ app/src/main/java/com/spoj/holamundo/FirstActivity.java
@@ -0,0 +1,13 @@
+package com.spoj.holamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
